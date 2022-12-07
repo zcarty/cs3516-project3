b81ac766e7070e31f6b347bad48bf0eec642e34c
zcarty <zeb0402@gmail.com>
2022-12-06 21:27:33 -0500

    can properly read from config file

--- config.cpp
+++ config.cpp
@@ -9,8 +9,7 @@ struct fromConfig {
     int TTLVal;
     string ip_host;
     string ip_overlay;
-    int sendDelay;
-    int recieveDelay;
+    int delay[7];
 };
 
 fromConfig config(int nodeID) {
@@ -29,7 +28,7 @@ fromConfig config(int nodeID) {
     // LINES 2-4
     int setupNode;
     char *ip_buff;
-    for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 3; i++){
         getline(configFile, buff);
         sscanf(buff.c_str(), "%d %d", &step, &setupNode);
         if (setupNode == nodeID) {
@@ -41,29 +40,42 @@ fromConfig config(int nodeID) {
     char *ip_buff2;
     for(int i = 0; i < 3; i++){
         getline(configFile, buff);
-        sscanf(buff.c_str(), "%d %d", step, setupNode);
-        if (setupNode[0] == nodeID) {
-            out.ip_host = ip_buff;
-            out.ip_overlay = ip_buff2;
+        sscanf(buff.c_str(), "%d %d", &step, &setupNode);
+        if (setupNode == nodeID) {
+            out.ip_host = buff.substr(4,10);
+            out.ip_overlay = buff.substr(15);
         }
     }
 
-    // work on delay (only need sendDelay (but for 2 other nodes))
-    // // LINES 8-11
-    // int sendNode;
-    // int recNode;
-    // int delayBuff;
-    // int recBuff;
-    // for(int i = 0; i < 3; i++){
-    //     getline(configFile, buff);
-    //     sscanf(buff.c_str(), "%d %d %d %d", step, sendNode, delayBuff, recNode, recBuff);
-    //     if (sendNode == nodeID) {
-    //         out.sendDelay
-    //     }
-    // }
+    // LINES 8-11
+    int sendNode;
+    int sendDelay;
+    int recNode;
+    int recDelay;
+    for(int i = 0; i < 3; i++){
+        getline(configFile, buff);
+        sscanf(buff.c_str(), "%d %d %d %d %d", &step, &sendNode, &sendDelay, &recNode, &recDelay);
+        if (sendNode == nodeID) {
+            out.delay[recNode] = sendDelay;
+        }
+        else if (recNode == nodeID) {
+            out.delay[sendNode] = recDelay;
+        }
+    }
 
     // LINES 11-13
-
+    char *serverOverlay;
+    for(int i = 0; i < 3; i++){
+        getline(configFile, buff);
+        sscanf(buff.c_str(), "%d %d %d", &step, &sendNode, &sendDelay);
+        sscanf(buff.substr(20).c_str(), "%d %d", &recNode, &recDelay);
+        if (sendNode == nodeID) {
+            out.delay[recNode] = sendDelay;
+        }
+        else if (recNode == nodeID) {
+            out.delay[sendNode] = recDelay;
+        }
+    }
 
     configFile.close();
     return out;
--- test.cpp
+++ test.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main(void) {
     fromConfig data = config(1);
     cout << data.ip_host << endl;
+    cout << data.ip_overlay << endl;
     cout << data.queueLength << endl;
+
+    cout << data.delay[2] << endl;
+    cout << data.delay[4] << endl;
 }
